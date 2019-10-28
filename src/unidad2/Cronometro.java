e8b449cb72119f1bc724278edd1a5ae2196b5ec1
mamglezcom <contacto@mamglez.com>
2019-10-28 20:13:12 +0100

    Ejercicio 3 de la unidad 2 resuelto

---
+++ src/unidad2/Cronometro.java
@@ -0,0 +1,43 @@
+package unidad2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cronometro {
+
+	public static void main(String[] args) throws IOException {
+		
+		// variables
+		String nombre ="";
+		double inicioContador;
+		double finalContador;
+		double resultadoEnMillisegundos;
+		
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader (isr);
+		
+		// capturamos momento actual en ms.
+		inicioContador = System.currentTimeMillis();
+		
+		// preguntamos por consola el nombre
+		System.out.println("Escribe tu nombre:");
+		nombre = br.readLine();
+		
+		//capturamos de nuevo el momento actual en ms.
+		finalContador= System.currentTimeMillis();
+		
+		//realizamos la resta en ms y dividimos entre mil para obtener segundos
+		resultadoEnMillisegundos = ((finalContador-inicioContador) / 1000);
+		
+		//sacamos por consola el resultado
+		System.out.print("Hola " + nombre + ", " + "has tardado ");
+		System.out.printf("%.3f",resultadoEnMillisegundos);
+		System.out.print(" segundos en decir tu nombre");
+		
+		
+
+	}
+
+}
