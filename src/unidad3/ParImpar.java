1edd5e9665203a958db4702a1ae5c872b7e8a600
mamglezcom <contacto@mamglez.com>
2019-11-04 20:04:02 +0100

    Ejercicio 1 de la unidad 3 resulto

--- src/unidad3/ParImpar.java
+++ src/unidad3/ParImpar.java
@@ -6,11 +6,13 @@ public class ParImpar {
 
 	public static void main(String[] args) {
 		
-		
+		// variables
 		int numero;
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("escribe un numero entero:");
 		numero = sc.nextInt();
+		// numero es par si el resto de dividirlo entre 2 es 0
 		if(numero % 2 == 0) {
 			System.out.println(numero + " es un numero par");
 		}else {
